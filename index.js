edb63c84c38769e18e523a7636e76c79214ac615
goedh452 <bg.goedhart@gmail.com>
2018-08-21 15:57:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ HttpSunscreen.prototype =
 	getTargetPosition: function(callback)
 	{
 		this.log("FUNCTION getTargetPosition");
-		this.SunscreenService.getCharacteristic(Characteristic.TargetPosition).setValue(0);
+		this.SunscreenService.getCharacteristic(Characteristic.TargetPosition).updateValue(0);
 		callback();
 	},
 	
